685490050e053fa77874779a188bd40b77a31d4b
Roustem Karimov <roustem@agilebits.com>
2016-11-22 14:16:55 -0500

    Add gcloud to the PATH

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -1,3 +1,9 @@
 
 set -xg GOPATH $HOME/go
-set -xg PATH $PATH $GOPATH/bin $HOME/bin
+
+set -xg GCLOUD_HOME $HOME/google-cloud-sdk
+set fish_user_paths $GCLOUD_HOME/bin
+ 
+set -x MANPATH GCLOUD_HOME/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
+
+set -xg PATH $PATH $GOPATH/bin $HOME/bin $GCLOUD_HOME/bin
